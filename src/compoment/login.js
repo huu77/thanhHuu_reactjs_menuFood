0e3cf50d40b4e7d4103c2c57ce70189d6ac285ff
huu77 <thuu28052002@gmail.com>
2022-09-16 15:13:05 +0700

    tiep theo

--- src/compoment/login.js
+++ src/compoment/login.js
@@ -1,9 +1,45 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import IconButton from "@mui/joy/IconButton";
+import Input from "@mui/joy/Input";
+import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import styles from '../scss/login.scss';
+import classNames from 'classnames/bind';
 
-function LOGIN() {
-    return <div>
-        LOGIN 
+
+export default function InputIntegration() {
+
+    const cx = classNames.bind(styles);
+  return (
+    <div className={cx('login_form')}>
+        <Box sx={{ display: "flex", flexDirection: "column" ,width:"300px",marginTop:"50px"}} >
+        <Input
+          size="lg"
+          startDecorator={<AccountCircleIcon />}
+          placeholder="Tai Khoan"
+          type="text"
+         
+        />
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column",width:"300px",marginTop:"50px"}}>
+        <Input
+          size="lg"
+          startDecorator={<KeyRoundedIcon />}
+          placeholder="Password"
+          type="password"
+          endDecorator={
+            <IconButton color="neutral">
+              <VisibilityRoundedIcon />
+            </IconButton>
+          }
+        />
+      </Box>
+      <Button variant="contained" color="success" sx={{margin:"30px",marginBottom:"50px"}}>
+        LOGIN
+      </Button>
     </div>
+  );
 }
-
-export default LOGIN ;
--- src/compoment/page/menuChild.js
+++ src/compoment/page/menuChild.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Data } from "../../data/db";
-
+import Swal from 'sweetalert2'
 import Button from "@mui/material/Button";
 function MenuChild() {
   const { slug } = useParams();
   const data = Data.filter((data) => data.slug === slug);
-
+const handleClick=()=>{
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
+}
   return (
     <div>
       {data.map((item) => {
@@ -33,7 +50,7 @@ function MenuChild() {
               <h1>{item.name}</h1>
               <p style={{ fontSize: "20px" }}>{item.mota}</p>
               <p style={{ fontSize: "16px" }}>GIÁ:{item.gia}</p>
-              <Button variant="contained" color="success">
+              <Button variant="contained" color="success" onClick={handleClick}>
                 Đặt ngay
               </Button>
             </div>
